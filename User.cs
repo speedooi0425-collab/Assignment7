88e79c7046afa5748d60cfc571c716c3b5acf633
speedooi0425-collab <speedooi0425@gmail.com>
2026-03-27 07:14:22 +0800

    Create User.cs

---
+++ User.cs
@@ -0,0 +1,9 @@
+namespace SafeVault.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; } // e.g., "Admin", "User"
+    }
+}
